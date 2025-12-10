903ce4ee015411492f702a74ac15645bc84e0248
KOUSTAV <koustavganguly24@gmail.com>
2025-12-10 23:54:09 +0530

    1 day complete

---
+++ Day1/datatypes.js
@@ -0,0 +1,63 @@
+/*
+5. Create datatypes.js file and use the JavaScript **_typeof_** operator to check different data types. Check the data type of each variable
+*/
+
+let str = "this is a string";
+let isLoggedIn = true;
+let itIsUndefined;
+let blank = null;
+
+console.log(typeof str);  //
+console.log(typeof isLoggedIn);
+console.log(typeof itIsUndefined);
+console.log(typeof blank);
+
+/*
+6. Declare four variables without assigning values
+*/
+let name;
+let agee;
+let coountry;
+let height;
+
+/*
+7. Declare four variables with assigned values
+*/
+
+let favFruite = "mango";
+let favOS = "windows (not true)";
+let isWindowsFav = false;
+let fish = "piranha";
+
+/*
+8. Declare variables to store your first name, last name, good or bad, country and age in multiple lines
+*/
+/*
+let firstName = "Koustav";
+let lastName = "Ganguly";
+let isGood = true;
+let country = "India";
+let agee = 53;
+*/
+
+/*
+9. Declare variables to store your first name, last name, good or bad, country and age in single lines
+*/
+
+
+let firstName = "Koustav", lastName = "Ganguly", maritalStatus = false, country = "India", age = 53;
+
+
+/*
+10. Declare two variables _myAge_ and _yourAge_ and assign them initial values and log to the browser console.
+
+```sh
+I am 25 years old.
+You are 30 years old.
+```
+*/
+
+let myAge=25
+let yourAge=30
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
---
+++ Day1/variable.js
@@ -0,0 +1,8 @@
+/*
+ 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+ */
+
+let str = "this is a string"; // string
+let isLoggedIn = true; // boolean
+let itIsUndefined; // undefined
+let blank = null; // object >> it is not object but js shows it because it is the problem of js not ours
